b24b7929f3a538d5f1037d4699bc9cab821ea13d
7onetella <7onetella@gmail.com>
2022-09-18 23:12:53 -0400

    clean-up

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+# make sure to do this before releasing 
+# export GITHUB_TOKEN=ghp_xxxxxxx <= github/7onetella Settings/Developer settings/personal access token
+# gorelease
+
+if [ "${1}" == "" ]; then
+  echo "specify tag version"
+  exit 1
+fi
+
+version=${1}
+
+git tag -a v${version} -m "${version} release"
+
+git push origin --tags
+
+goreleaser release --rm-dist
